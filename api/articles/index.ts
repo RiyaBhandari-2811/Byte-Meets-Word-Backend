d88c951d40ad35684cf3f7ce0f4365f0ca259f36
Riya <bhandari28riya@gmail.com>
2025-06-17 20:40:34 +0530

    refactor: add CORS headers to OPTIONS request handling

--- api/articles/index.ts
+++ api/articles/index.ts
@@ -62,6 +62,19 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       }
       break;
 
+    case "OPTIONS":
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,OPTIONS"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      res.status(200).end();
+      return;
+
     default:
       logger.debug(`Unsupported HTTP method: ${method}`);
       res.setHeader("Allow", ["POST", "GET", "PATCH", "DELETE"]);
